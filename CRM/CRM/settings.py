67eefe9b7a61e2b24a276ced2cbae68900deb3a8
ZakirAlekperov <zakiralekperov1998.com>
2024-09-30 15:48:50 +0300

    первый запуск

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin',
     'analytics',
-    'inventoty'
+    'inventory',
     'notifications',
     'orders',
     'reports',
